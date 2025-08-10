c328e49ecb9db6f838a38d6c17f7bbe7d9650072
Alvaro Atias <aloatias88@gmail.com>
2025-08-11 01:35:26 +0200

    Init project (#1)
    Copy project from: https://github.com/emilybache/GildedRose-Refactoring-Kata/tree/main/csharp.xUnit
    Fix unit tests

---
+++ tests/GildedRoseTest.cs
@@ -0,0 +1,17 @@
+﻿using GildedRose.Core;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GildedRoseTests;
+
+public class GildedRoseTest
+{
+    [Fact]
+    public void foo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var app = new GildedRose.Core.GildedRose(Items);
+        app.UpdateQuality();
+        Assert.Equal("foo", Items[0].Name);
+    }
+}
